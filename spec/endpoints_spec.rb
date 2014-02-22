f1841fea41204fb18e7c956da36deddfa8664932
Ryan Gasparini <ryan@rxgx.com>
2014-02-22 04:46:10 -0700

    Fixed the encoding and escaping scenarios in endpoints spec

--- spec/endpoints_spec.rb
+++ spec/endpoints_spec.rb
@@ -3,28 +3,21 @@ require 'spec_helper'
 describe GrepwordsClient::Endpoints do
 
   let(:config)        { GrepwordsClient.config }
-  let(:keywords_good) { ['apple', 'ipad'] }
-  let(:keywords_bad)  { ['|', '\n'] }
+  let(:keywords_good) { ['apple', 'ipad air', 'http://google.com/?q=sports+cards&lang=en-us'] }
+  let(:keywords_bad)  { %w(| \n) }
 
   describe 'setup keywords' do
 
-    it 'takes an array of keywords and makes newline separated string' do
-      keywords = GrepwordsClient::Endpoints.encode_keywords(keywords_good)
-      keywords.should be_an_instance_of(String)
-      keywords.should eql 'apple|ipad'
-    end
-
     it 'encodes keyword names correctly' do
-      keywords = GrepwordsClient::Endpoints.encode_keywords(['baseball+cards', 'football cards', 'http://google.com/?q=sports+cards&lang=eng'])
-      keywords.should be_an_instance_of(String)
-      keywords.should eql 'baseball%2Bcards|football+cards|http%3A%2F%2Fgoogle.com%2F%3Fq%3Dsports%2Bcards%26lang%3Deng'
+      keywords = GrepwordsClient::Endpoints.escape_keywords(keywords_good)
+      expect(keywords).to be_an_instance_of String
+      expect(keywords).to eq 'apple%7Cipad+air%7Chttp%3A%2F%2Fgoogle.com%2F%3Fq%3Dsports%2Bcards%26lang%3Den-us'
     end
 
-    it 'sets max keyword length to 3950' do
-      terms = []
-      1.upto(4000) { |i| terms << "keyword #{i}" }
-      keywords = GrepwordsClient::Endpoints.encode_keywords(terms)
-      keywords.length.should eql 3950
+    it 'correctly encodes url' do
+      keywords = GrepwordsClient::Endpoints.escape_keywords(keywords_good)
+      url = GrepwordsClient::Endpoints.encode_url('lookup', keywords)
+      expect(url).to eq 'http://api.grepwords.com/lookup?apikey=&q=apple%7Cipad+air%7Chttp%3A%2F%2Fgoogle.com%2F%3Fq%3Dsports%2Bcards%26lang%3Den-us'
     end
 
   end
@@ -33,18 +26,25 @@ describe GrepwordsClient::Endpoints do
 
     before { set_config }
 
+    it 'sets max keyword length to 3950' do
+      terms = []
+      1.upto(4000) { |i| terms << "keyword #{i}" }
+      keywords = GrepwordsClient::Endpoints.lookup(terms)
+      expect(keywords).to eq nil
+    end
+
     context 'valid keywords', vcr: { cassette_name: 'keyword_tool/lookup/successfully/valid', record: :none } do
 
-      subject(:response) { GrepwordsClient::Endpoints.lookup(keywords_good) }
+      subject { GrepwordsClient::Endpoints.lookup(keywords_good) }
 
-      it { should be_an_instance_of Hash }
-      it { should have_key 'apple' }
-      it { should have_key 'ipad' }
+      it { expect be_an_instance_of Hash }
+      it { expect have_key 'apple' }
+      it { expect have_key 'ipad air' }
 
       it 'returns correct properties' do
         %w(cpc cmp gms lms m1 m2 m3 m4 m5 m6 m7 m8 m9 m10 m11 m12).each do |property|
-          expect(response['apple']).to include property
-          expect(response['ipad']).to include property
+          expect(subject['apple']).to have_key property
+          expect(subject['ipad air']).to have_key property
         end
       end
 
@@ -54,13 +54,13 @@ describe GrepwordsClient::Endpoints do
 
       subject(:response) { GrepwordsClient::Endpoints.lookup(keywords_bad) }
 
-      it { should be_an_instance_of Hash }
-      it { should have_key '|' }
-      it { should have_key '\n' }
+      it { expect be_an_instance_of Hash }
+      it { expect have_key '|' }
+      it { expect have_key '\n' }
 
       it 'returns nil values' do
-        expect(response['|']).to be nil
-        expect(response['\n']).to be nil
+        expect(response['|']).to eq nil
+        expect(response['\n']).to eq nil
       end
 
     end
